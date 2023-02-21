97329d0f6d0b6bda9468e1337115ed91873ab0c5
rumeysaguc <rmys.guc@hotmail.com>
2023-02-21 14:00:24 +0200

    bugfix

---
+++ test.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from selenium import webdriver
+from lxml import etree
+
+def crawling(url, path):
+    outfile = open('result2.json', 'w')
+    print(url)  
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content,
+                         "html.parser")
+    #dom = etree.HTML(str(soup))
+    dom = soup.find_all("div", {"class": "entry-content est-content-1"})
+    for i in dom:
+        print( i.find('p').text)
+        data = {
+            "text": i.find('p').text
+        }
+    json.dump(data, outfile)
+    #print(dom.xpath(path))
+  
+
+url = "https://www.olay.com.tr/kunye"
+path = '/html/body/div[5]/div[2]/div[4]'
+crawling(url,path)
